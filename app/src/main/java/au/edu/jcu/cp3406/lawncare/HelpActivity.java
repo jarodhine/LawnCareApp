607a9da96ac53c17923c073527329d6085838ee4
Dylan-Wharton <dylan.wharton@my.jcu.edu.au>
2020-05-18 22:59:52 +1000

    Clicking 'help' now displays a pdf, testpdf used as placeholder

--- app/src/main/java/au/edu/jcu/cp3406/lawncare/HelpActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/lawncare/HelpActivity.java
@@ -3,13 +3,18 @@ package au.edu.jcu.cp3406.lawncare;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+
+import com.github.barteksc.pdfviewer.PDFView;
 
 public class HelpActivity extends AppCompatActivity {
 
+    PDFView pdfView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+
+        pdfView = findViewById(R.id.pdfView);
+        pdfView.fromAsset("testpdf.pdf").load();
     }
 }
