7df02d09a994a051863a83e33065548af7b5d9cc
Dylan-Wharton <dylan.wharton@my.jcu.edu.au>
2020-05-27 14:08:11 +1000

    Add functionality to display correct pdf according to system language

--- app/src/main/java/au/edu/jcu/cp3406/lawncare/HelpActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/lawncare/HelpActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import com.github.barteksc.pdfviewer.PDFView;
 
+import java.util.Locale;
+
 public class HelpActivity extends AppCompatActivity {
 
     PDFView pdfView;
@@ -14,7 +16,32 @@ public class HelpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
 
+        String default_language = Locale.getDefault().getDisplayLanguage();
+        String asset_name = "maintain_your_lawn_mower.pdf";
+
+        switch (default_language){
+            case "English":
+                asset_name = "maintain_your_lawn_mower.pdf";
+                break;
+            case "Deutsch":
+                asset_name = "maintain_your_lawn_mower_german.pdf";
+                break;
+            case "italiano":
+                asset_name = "maintain_your_lawn_mower_italian.pdf";
+                break;
+            case "日本語":
+                asset_name = "maintain_your_lawn_mower_japanese.pdf";
+                System.out.println("TESTING JAPANESE");
+                break;
+            case "中文":
+                asset_name = "maintain_your_lawn_mower_mandarin.pdf";
+                break;
+            case "ਪੰਜਾਬੀ":
+                asset_name = "maintain_your_lawn_mower_punjabi.pdf";
+                break;
+        }
+
         pdfView = findViewById(R.id.pdfView);
-        pdfView.fromAsset("maintain_your_lawn_mower.pdf").load();
+        pdfView.fromAsset(asset_name).load();
     }
 }
